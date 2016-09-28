621fc36502a729c2dbfaaad06bb355f22a74a2da
idedes <irene.dedes@iphc.cnrs.fr>
2016-09-28 15:32:25 +0200

    data

---
+++ ws_exper/levels.d
@@ -0,0 +1,47 @@
+This file contains single-particle levels grouped in shells
+
+(j=1 means spin up, j=0 means spin down)
+
+n>>l>>j
+
+1  0  1
+
+1  1  1
+1  1  0
+
+1  2  1
+2  0  1
+1  2  0
+
+1  3  1
+
+2  1  1
+1  3  0
+2  1  0
+
+1  4  1
+
+1  4  0
+2  2  1
+2  2  0
+3  0  1
+1  5  1
+
+1  5  0
+2  3  1
+2  3  0
+3  1  1
+3  1  0
+1  6  1
+
+2  4  1
+3  2  1
+1  6  0
+2  4  0
+4  0  1
+3  2  0
+1  7  1
+
+
+ENDFILE
+
---
+++ ws_exper/levels.new.d
@@ -0,0 +1,38 @@
+This file contains single-particle levels grouped in shells
+
+n>>l>>j
+
+1  0  1   1s1/2   Nshell=0
+1  1  3   1p3/2   Nshell=1
+1  1  1   1p1/2    = " =
+1  2  5   1d5/2   Nshell=2
+2  0  1   2s1/2    = " =
+1  2  3   1d3/2    = " =
+1  3  7   1f7/2   Intruder Nshell=3
+2  1  3   2p3/2   Nshell=3
+1  3  5   1f5/2    = " =
+2  1  1   2p1/2    = " =
+1  4  9   1g9/2   Intruder Nshell=4
+1  4  7   1g7/2   Nshell=4
+2  2  5   2d5/2    = " =
+2  2  3   2d3/2    = " =
+3  0  1   3s1/2    = " =
+1  5  11  1h11/2  Intruder Nshell=5
+1  5  9   1h9/2   Nshell=5
+2  3  7   2f7/2    = " =
+2  3  5   2f5/2    = " =
+3  1  3   3p3/2    = " =
+3  1  1   3p1/2    = " =
+1  6  13  1i13/2  Intruder Nshell=6
+2  4  9   2g9/2   Nshell=6
+3  2  5   3d5/2    = " =
+1  6  11  1i11/2   = " =
+2  4  7   2g7/2    = " =
+4  0  1   4s1/2    = " =
+3  2  3   3d3/2    = " =
+1  7  15  1j15/2  Intruder Nshell=6
+
+ENDFILE
+
+This file contains 29 active lines
+
